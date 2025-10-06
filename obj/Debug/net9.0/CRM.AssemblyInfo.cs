d469b90911fd1c45e072d80aa940d008508a2d6c
Chen Prom <cprom@MacBookPro.lan>
2025-10-05 20:05:48 -0700

    updates client page styling

--- obj/Debug/net9.0/CRM.AssemblyInfo.cs
+++ obj/Debug/net9.0/CRM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46d7d680c8b4da22e6b2f47d80241401022cba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8f28d4fd1a0a338b7e9d4864806756e752db9f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
