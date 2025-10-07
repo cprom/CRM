2829269f6bb2e2115111b9d0f72259dc02e522bc
Chen Prom <cprom@MacBookPro.lan>
2025-10-06 21:16:52 -0700

    ad delete dialog page

--- obj/Debug/net9.0/CRM.AssemblyInfo.cs
+++ obj/Debug/net9.0/CRM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32158e969f88cf177a302290fab079848f4590ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3136c292f81d1ca9dd297b5bcd98e0c958207ff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
